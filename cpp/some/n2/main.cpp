8ecd1dd23bfff957c8e88a146a2e4472723612c0
Артемьев Леонид Сергеевич <leonidart14@gmail.com>
2023-07-31 16:49:16 +0700

    something small

--- cpp/some/n2/main.cpp
+++ cpp/some/n2/main.cpp
@@ -1,8 +1,24 @@
 #include <vector>
+#include <algorithm>
+#include <iterator>
 #include <iostream>
+
+using namespace std;
+
+void print_vec(const vector<int>& vec)
+{
+    copy(vec.begin(), vec.end(),
+         ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
 
 int main()
 {
-
+    vector vec{1, 2, 3, 4};
+    print_vec(vec);
+    iter_swap(vec.begin() + 0, vec.begin() + 1); // swap vals.
+    print_vec(vec);
+    iter_swap(vec.begin() + 1, vec.begin() + 3);
+    print_vec(vec);
     return 0;
 }
