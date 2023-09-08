39dc3ed7ec3baecc8fb8eda7546f8b509505dd34
Артемьев Леонид Сергеевич <leonidart14@gmail.com>
2023-09-08 16:42:39 +0700

    some parts

--- cpp/some/n46/main.cpp
+++ cpp/some/n46/main.cpp
@@ -1,10 +1,17 @@
-#include <QCoreApplication>
+#include <QList>
+#include <QVector>
 
-// QList vs QVector
+#include <QObject>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    QObject obj;
 
-    return a.exec();
+    QList<QObject> objList;
+    objList.push_back(obj);
+
+    QVector<QObject> objVec;
+    objVec.push_back(obj);
+
+    return 0;
 }
