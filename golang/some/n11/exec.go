537438467bf58fb388f7710df85786f02064c789
Leonid Artemev <leonidart14@gmail.com>
2023-10-05 02:01:13 +0700

    task from go training

--- golang/some/n11/exec.go
+++ golang/some/n11/exec.go
@@ -42,8 +42,57 @@ func generateArray_3(length int) []int {
 	return result
 }
 
+func countNumbers(data []int) string {
+	/*
+	   уникальных чисел
+	   дублированных чисел
+	   нулей
+	   четных чисел
+	   нечетных чисел
+	*/
+
+	zeroCount := 0
+	mod2Eq0Count := 0
+	mod2Eq1Count := 0
+	mp := make(map[int]int)
+	for _, val := range data {
+		if val == 0 {
+			zeroCount++
+		}
+
+		if val != 0 {
+			if val%2 == 0 {
+				mod2Eq0Count++
+			} else {
+				mod2Eq1Count++
+			}
+		}
+
+		mp[val]++
+	}
+
+	uniqueCount := 0
+	duplicateCount := 0
+
+	for _, val := range mp {
+		if val == 1 {
+			uniqueCount++
+		} else {
+			duplicateCount++
+		}
+	}
+
+	return fmt.Sprintf(
+		"%v, %v, %v, %v, %v",
+		uniqueCount, duplicateCount,
+		zeroCount,
+		mod2Eq0Count, mod2Eq1Count,
+	)
+}
+
 func Exec() {
+	arr := []int{1, 2, 3, 1, 2, 5, 4, 1, 2, 0, 0, 0}
 	fmt.Println(
-		generateArray_3(
-			5))
+		countNumbers(
+			arr))
 }
