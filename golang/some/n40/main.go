2ae91301ac60754e5f05994a82f4ca72d2089345
Leonid Artemev <leonidart14@gmail.com>
2024-09-08 23:46:46 +0700

    upd golang examples

--- golang/some/n40/main.go
+++ golang/some/n40/main.go
@@ -12,6 +12,17 @@ func Max[T cmp.Ordered](a, b T) T {
 	return b
 }
 
+func ZipMap[Key comparable, Val any](
+	keys []Key, vals []Val) map[Key]Val {
+
+	resultMap := map[Key]Val{}
+	maxLen := min(len(keys), len(vals))
+	for i := 0; i < maxLen; i++ {
+		resultMap[keys[i]] = vals[i]
+	}
+	return resultMap
+}
+
 func main() {
 	{
 		a, b := 5, 3
@@ -22,5 +33,19 @@ func main() {
 		a, b := 5.5, 3.3
 		max := Max(a, b)
 		fmt.Println(max) // 5.5
+	}
+
+	{
+		m := ZipMap([]string{"one"}, []int{11, 22, 33})
+		fmt.Println(m)
+		// map[one:11]
+	}
+	{
+		keys := []string{"one", "two", "thr"}
+		vals := []int{11, 22, 33}
+
+		m := ZipMap(keys, vals)
+		fmt.Println(m)
+		// map[one:11 two:22 thr:33]
 	}
 }
